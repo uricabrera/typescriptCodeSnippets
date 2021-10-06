e65aa4fc5c85628a9449055cc6edbcf361453729
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-06 13:04:11 -0300

    added annotations example

---
+++ annotations/variables.ts
@@ -0,0 +1,37 @@
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ["red","green","blue"];
+let myNumbers: number[] = [4,1,5,24];
+let truths: boolean[] = [true,false,true,false];
+
+// Classes
+
+class Car{
+
+}
+
+let car: Car = new Car();
+
+// Object Literal
+
+let point: {x: number; y: number} = {
+    x: 10,
+    y: 20
+}
+
+// Function
+
+const logNumber: (i: number) => void = (i: number) => {
+    console.log(i);
+};
+
