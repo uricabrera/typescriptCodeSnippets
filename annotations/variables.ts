3daf162c13646040dac8e7d1b6226f07e7586397
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-06 13:47:02 -0300

    2. do not use type inference when we want to initialize a variable later on after declaring it

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -43,3 +43,16 @@ const json = '{"x": 10,"y":20}';
 const coordinates: {x:number;y:number} = JSON.parse(json);
 console.log(coordinates);
 
+// 2) When we declare a variable on one line and then initialize it later
+
+let words = ["red","green","blue"];
+let foundWord: boolean;
+
+for(let i=0;i<words.length;i++){
+    if(words[i] === "green"){
+        foundWord = true;
+        console.log(i);
+        break;
+    }
+}
+
